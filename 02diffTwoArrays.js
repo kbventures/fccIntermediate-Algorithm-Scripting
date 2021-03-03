1eee5f3eb7b93a29890dd651b66ae150583918b9
kbventures <kmb_x@hotmail.com>
2021-03-03 22:20:01 +0100

    2 Diff Arrays declarative attempt and solutions

--- 02diffTwoArrays.js
+++ 02diffTwoArrays.js
@@ -1,4 +1,4 @@
-// Imperative Solution
+Imperative Solution
 
 function diffArray(arr1, arr2) {
   var newArr = [];
@@ -19,7 +19,41 @@ function diffArray(arr1, arr2) {
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
-//Declarative Solution
+
+
+//Declarative Attempt
+function diffArrayDeclarative(arr1, arr2) {
+  var newArr = [];
+  const concatArr1And2 = arr1.concat(arr2);
+  const solution = concatArr1And2.filter(x=>{
+    return arr2.indexOf(x) === -1 || arr1.indexOf(x) === -1;
+  })
+  return newArr;
+}
+
+diffArrayDeclarative([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+
+
+// Declarative solution 1
+// function diffArray(arr1, arr2) {
+//   return arr1
+//     .concat(arr2)
+//     .filter(item => !arr1.includes(item) || !arr2.includes(item));
+// }
+
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+// Declarative Solution 2
+// function diffArray(arr1, arr2) {
+//   return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+//   function diff(a, b) {
+//     return a.filter(item => b.indexOf(item) === -1);
+//   }
+// }
 
 
 
