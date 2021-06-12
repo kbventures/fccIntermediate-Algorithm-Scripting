3d4513f154df95c967bebb21d6a4c78f5c8e1d63
kbventures <kmb_x@hotmail.com>
2021-06-12 14:48:17 +0200

    FCC Test and Automated Test Working

--- 03seekAndDestroy/03seekAndDestroy.js
+++ 03seekAndDestroy/03seekAndDestroy.js
@@ -1,11 +1,49 @@
 function removeFromArrayArgumentsOfSameValue(initialArrayAndArguments) {
   const argumentsArray = Array.prototype.slice.call(arguments);
+
+  // Seperate Initial Array From Arguments to be removed
+  const elementsToBeRemoved = argumentsArray.splice(1);
   const initialArray = argumentsArray[0];
-  const elementsToBeRemovedFromInitalArray = [];
-  const stub = [1, 1];
-  return stub;
+
+  return initialArray.filter(
+    (initalArrayElement) => !elementsToBeRemoved.includes(initalArrayElement)
+  );
 }
 
 removeFromArrayArgumentsOfSameValue([1, 2, 3, 1, 2, 3], 2, 3);
 
 module.exports = removeFromArrayArgumentsOfSameValue;
+
+/*
+Alternate Solution from FCC 1
+
+function destroyer(arr) {
+  let valsToRemove = Object.values(arguments).slice(1);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        delete arr[i];
+      }npm 
+    }
+  }
+  return arr.filter(item => item !== null);
+}
+
+
+Alternate Solution from FCC 2
+
+function destroyer(arr) {
+  var valsToRemove = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !valsToRemove.includes(val);
+  });
+}
+
+
+Alternate Solution from FCC 3
+
+function destroyer(arr, ...valsToRemove) {
+  return arr.filter(elem => !valsToRemove.includes(elem));
+}
+*/
--- 03seekAndDestroy/test/test.seekAndDestroy.js
+++ 03seekAndDestroy/test/test.seekAndDestroy.js
@@ -1,10 +1,3 @@
-/*
-You will be provided with an initial array (the first argument
-in the destroyer function), followed by one or more 
-arguments. Remove all elements from the initial array that
-are of the same value as these arguments.
-*/
-
 const chai = require('chai');
 const expect = chai.expect;
 const assertArray = require('chai-arrays');
@@ -13,38 +6,71 @@ const seekAndDestroy = require('../03seekAndDestroy');
 
 describe('Seek and Destroy Test Suite', () => {
   it('seekAndDestroy([1,2,3,1,2,3], 2,3) should return array [1,1]', () => {
-    let array = [1, 2, 3, 1, 2, 3];
-
     const seekAndDestroytReturnedResult = seekAndDestroy(
       [1, 2, 3, 1, 2, 3],
       2,
       3
     );
-    return expect(seekAndDestroytReturnedResult).to.be.eql([1, 1]);
+    expect(seekAndDestroytReturnedResult).to.be.eql([1, 1]);
   });
 
   it('seekAndDestroy([1,2,3,5,1,2,3], 2,3) should return array [1,5,1]', () => {
-    let array = [1, 2, 3, 5, 1, 2, 3];
-    return null;
+    const seekAndDestroytReturnedResult = seekAndDestroy(
+      [1, 2, 3, 5, 1, 2, 3],
+      2,
+      3
+    );
+    expect(seekAndDestroytReturnedResult).to.be.eql([1, 5, 1]);
   });
 
   it('seekAndDestroy([3,5,1,2,2], 2,3,5) should return array [1]', () => {
-    let array = [3, 5, 1, 2, 2];
-    return null;
+    const seekAndDestroytReturnedResult = seekAndDestroy(
+      [3, 5, 1, 2, 2],
+      2,
+      3,
+      5
+    );
+    expect(seekAndDestroytReturnedResult).to.be.eql([1]);
   });
 
   it('seekAndDestroy([2,3,2,3],2,3) should return array []', () => {
-    let array = [2, 3, 2, 3];
-    return null;
+    const seekAndDestroytReturnedResult = seekAndDestroy([2, 3, 2, 3], 2, 3);
+    expect(seekAndDestroytReturnedResult).to.be.eql([]);
   });
 
   it('seekAndDestroy(["tree","hamburger",53],"tree",53) should return array ["hamburger"]', () => {
-    let array = [1, 2, 3, 1, 2, 3];
-    return null;
+    const seekAndDestroytReturnedResult = seekAndDestroy(
+      ['tree', 'hamburger', 53],
+      'tree',
+      53
+    );
+    expect(seekAndDestroytReturnedResult).to.be.eql(['hamburger']);
   });
 
   it('seekAndDestroy(["possum","trollo",12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan","yatch"], "yatch", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"] should return array [12,92,65]', () => {
-    let array = [1, 2, 3, 1, 2, 3];
-    return null;
+    const seekAndDestroytReturnedResult = seekAndDestroy(
+      [
+        'possum',
+        'trollo',
+        12,
+        'safari',
+        'hotdog',
+        92,
+        65,
+        'grandma',
+        'bugati',
+        'trojan',
+        'yatch',
+      ],
+      'yatch',
+      'possum',
+      'trollo',
+      'safari',
+      'hotdog',
+      'grandma',
+      'bugati',
+      'trojan'
+    );
+    expect(seekAndDestroytReturnedResult).to.be.eql([12, 92, 65]);
   });
 });
