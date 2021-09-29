d4087de59d07febb0fdd6503b78ab67e23408dbf
kbventures <kmb_x@hotmail.com>
2021-09-29 18:25:51 +0200

    Steam Roller Challenge 16 Completed

--- 16steamRoller/steamRoller.js
+++ 16steamRoller/steamRoller.js
@@ -1,7 +1,15 @@
 function steamrollArray(arr) {
-  return arr;
+  const flattenedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenedArray.push(...steamrollArray(arr[i]));
+    } else {
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+// steamrollArray([1, [2], [3, [[4]]]]);
 
 module.exports = steamrollArray;
--- 16steamRoller/test/test.steamRoller.js
+++ 16steamRoller/test/test.steamRoller.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 
 describe('Streamroller test suite', () => {
   it('steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].', () => {
-    const result = steamrollArray([[['a']][['b']]]);
+    const result = steamrollArray([[['a']], [['b']]]);
     expect(result).to.eql(['a', 'b']);
   });
 
@@ -13,8 +13,13 @@ describe('Streamroller test suite', () => {
     expect(result).to.eql([1, 2, 3, 4]);
   });
 
-  it('steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].', () => {
-    const result = steamrollArray([1, [2], [3, [[4]]]]);
-    expect(result).to.eql([1, 2, 3, 4]);
+  it('steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4]j', () => {
+    const result = steamrollArray([1, [], [3, [[4]]]]);
+    expect(result).to.eql([1, 3, 4]);
+  });
+
+  it('steamrollArray() should return [1, {}, 3, 4]', () => {
+    const result = steamrollArray([1, {}, [3, [[4]]]]);
+    expect(result).to.eql([1, {}, 3, 4]);
   });
 });
