f5ccc294812a69f678e15c3cfde1f1a3a78fda03
kbventures <kmb_x@hotmail.com>
2021-10-21 22:07:42 +0200

    First Refactored Telephone Number Validator Attempt

--- telephonenumbervalidatorproject04/telephonenumbervalidator.js
+++ telephonenumbervalidatorproject04/telephonenumbervalidator.js
@@ -1,103 +1,139 @@
 'use strict';
 
-// function telephoneCheck(str) {
-//   let first3AreNumbers = /^\d{10}/;
-
-//   // const result = first3AreNumbers.test(str);
-
-//   var checkForSpecialCharacters = /^[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-//   // const test = str.test(
-
-//   const result = checkForSpecialCharacters.test(str);
-//   console.log(str);
-
-//   const resultRemovedSpecialCharacters = str.replace(
-//     /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g,
-//     ''
-//   );
-//   console.log(resultRemovedSpecialCharacters);
-
-//   if (result) {
-//     return false;
-//   }
-//   return true;
-// }
-
-// telephoneCheck('(55555  !55555');
-
-// module.exports = telephoneCheck;
-
-/*
-Special characters at beginning makes it false
-Important conditions & task
-Remove all special characters
-Remove Spaces
-Greater than 11 and less than 10 is false
-If exackly 11 digits only 1
-
-Check how many digits
-If certain amount of digit check if first digit equals 1
-
-
-
-
-No special characters at beginning
-First 11 or 10 characters
-If 11 only 1 as first digit
-Finally onlly certain formats are accepted
-555-555-5555
-5555555555
-555-555-5555
-(555)555-5555
-
-
-1 555-555-5555
-1 (555) 555-5555
-1(555)555-5555
-1 555 555 5555
-1 456 789 4444
-*/
-
 function telephoneCheck(str) {
-  const resultRemovedSpecialCharacters = str.replace(
+  const removedAllSpecialCharacters = str.replace(
     /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g,
     ''
   );
 
-  const isNum = /^\d+$/.test(str);
+  const isNum = /^[0-9]+$/.test(str);
 
-  if (resultRemovedSpecialCharacters.length < 10) {
+  if (removedAllSpecialCharacters.length < 10) {
     return false;
-  } else if (resultRemovedSpecialCharacters.length === 11 && str[0] !== '1') {
+  } else if (removedAllSpecialCharacters.length === 11 && str[0] !== '1') {
     return false;
-  } else if (resultRemovedSpecialCharacters.length === 10) {
+  } else if (removedAllSpecialCharacters.length === 10) {
+    //
     const is3digitsHyphen3digitsHyphen4Digits =
-      /^[0-9][0-9][0-9][-][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]/.test(str);
+      /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}/.test(str);
+
+    const isParenthesis3digitsParenthesis3digitsHyphen4Digits =
+      /^[(][0-9]{3}[)][0-9]{3}[-][0-9]{4}/.test(str);
+    //
     if (isNum) {
       return true;
     } else if (is3digitsHyphen3digitsHyphen4Digits) {
       return true;
+    } else if (isParenthesis3digitsParenthesis3digitsHyphen4Digits) {
+      return true;
     }
 
     return false;
-  } else if (resultRemovedSpecialCharacters.length === 11) {
-    const isSingledigitSpace3digitsSpace4Digits =
-      /^[1]\s[0-9][0-9][0-9]\s[0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9]/.test(str);
+  } else if (removedAllSpecialCharacters.length === 11) {
+    const isSingleDigitSpace3DigitsSpaceDigitsSpace4Digits =
+      /^[1]\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}/.test(str);
 
-    if (isSingledigitSpace3digitsSpace4Digits) {
+    const isSingleDigitSpace3DigitsHyphen3DigitsHyphen4Digits =
+      /^[1]\s[0-9]{3}[-][0-9]{3}[-][0-9]{4}/.test(str);
+
+    const isSingleDigitsSpaceParenthesis3DigitsParenthesisSpace3DigitsHyphen4Digits =
+      /^[1]\s[(][0-9]{3}[)]\s[0-9]{3}[-][0-9]{4}/.test(str);
+
+    const isSingleDigitParenthesis3DigitsParenthesis3DigitsHyphen4Digits =
+      /^[1][(][0-9]{3}[)][0-9]{3}[-][0-9]{4}/.test(str);
+
+    if (isSingleDigitSpace3DigitsSpaceDigitsSpace4Digits) {
       return true;
     }
-    console.log('test2');
+
+    if (isSingleDigitSpace3DigitsHyphen3DigitsHyphen4Digits) {
+      return true;
+    }
+    if (
+      isSingleDigitsSpaceParenthesis3DigitsParenthesisSpace3DigitsHyphen4Digits
+    ) {
+      return true;
+    }
+
+    if (isSingleDigitParenthesis3DigitsParenthesis3DigitsHyphen4Digits) {
+      return true;
+    }
+    return false;
   } else {
     return false;
   }
 }
 
-telephoneCheck('1 555 555 5555');
+console.log(telephoneCheck('1 555)555-5555'));
 
 module.exports = telephoneCheck;
 
-// 555-555-5555
-// 5555555555 DONE
-// 555-555-5555
-// (555)555-5555
+('use strict');
+
+function telephoneCheck(str) {
+  const removedAllSpecialCharacters = str.replace(
+    /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g,
+    ''
+  );
+
+  const isNum = /^[0-9]+$/.test(str);
+
+  if (removedAllSpecialCharacters.length < 10) {
+    return false;
+  } else if (removedAllSpecialCharacters.length === 11 && str[0] !== '1') {
+    return false;
+  } else if (removedAllSpecialCharacters.length === 10) {
+    //
+    const is3digitsHyphen3digitsHyphen4Digits =
+      /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}/.test(str);
+
+    const isParenthesis3digitsParenthesis3digitsHyphen4Digits =
+      /^[(][0-9]{3}[)][0-9]{3}[-][0-9]{4}/.test(str);
+    //
+    if (isNum) {
+      return true;
+    } else if (is3digitsHyphen3digitsHyphen4Digits) {
+      return true;
+    } else if (isParenthesis3digitsParenthesis3digitsHyphen4Digits) {
+      return true;
+    }
+
+    return false;
+  } else if (removedAllSpecialCharacters.length === 11) {
+    const isSingleDigitSpace3DigitsSpaceDigitsSpace4Digits =
+      /^[1]\s[0-9]{3}\s[0-9]{3}\s[0-9]{4}/.test(str);
+
+    const isSingleDigitSpace3DigitsHyphen3DigitsHyphen4Digits =
+      /^[1]\s[0-9]{3}[-][0-9]{3}[-][0-9]{4}/.test(str);
+
+    const isSingleDigitsSpaceParenthesis3DigitsParenthesisSpace3DigitsHyphen4Digits =
+      /^[1]\s[(][0-9]{3}[)]\s[0-9]{3}[-][0-9]{4}/.test(str);
+
+    const isSingleDigitParenthesis3DigitsParenthesis3DigitsHyphen4Digits =
+      /^[1][(][0-9]{3}[)][0-9]{3}[-][0-9]{4}/.test(str);
+
+    if (isSingleDigitSpace3DigitsSpaceDigitsSpace4Digits) {
+      return true;
+    }
+
+    if (isSingleDigitSpace3DigitsHyphen3DigitsHyphen4Digits) {
+      return true;
+    }
+    if (
+      isSingleDigitsSpaceParenthesis3DigitsParenthesisSpace3DigitsHyphen4Digits
+    ) {
+      return true;
+    }
+
+    if (isSingleDigitParenthesis3DigitsParenthesis3DigitsHyphen4Digits) {
+      return true;
+    }
+    return false;
+  } else {
+    return false;
+  }
+}
+
+console.log(telephoneCheck('1 555)555-5555'));
+
+module.exports = telephoneCheck;
