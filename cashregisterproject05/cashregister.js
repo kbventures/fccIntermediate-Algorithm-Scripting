f7cbc62db31b3b1068e87a80069e95c44bbaf5e1
kbventures <kmb_x@hotmail.com>
2021-12-28 05:16:15 +0200

    Improved Naming Convention, Remove Uncessary Variables and reduced uncessary code

--- cashregisterproject05/cashregister.js
+++ cashregisterproject05/cashregister.js
@@ -1,133 +1,142 @@
-function checkCashRegister(price, cash, cid) {
-  let change_Return_Object = { status: 'OPEN', change: [] };
-  let change_Due = cash - price;
-  let temp_Change;
-  let temp_Multiple;
-  let sum_of_cash_register = 0;
-  let denominatorNumber;
-  let writtenDenominator = '';
-  let cidIndex;
-
-  cid.forEach((element) => {
-    sum_of_cash_register = sum_of_cash_register += element[1];
+function checkCashRegister(purchasePrice, cashReceived, cashDrawerArr) {
+  const STATUS_AND_CHANGE_DUE = { status: 'OPEN', change: [] };
+  let changeDue = cashReceived - purchasePrice;
+  let sumCashRegister = 0;
+  const LEGAL_TENDER_ARR = [
+    ['ONE HUNDRED', 100],
+    ['TWENTY', 20],
+    ['TEN', 10],
+    ['FIVE', 5],
+    ['ONE', 1],
+    ['QUARTER', 0.25],
+    ['DIME', 0.1],
+    ['NICKEL', 0.05],
+    ['PENNY', 0.01],
+  ];
+  cashDrawerArr.forEach((denominations) => {
+    sumCashRegister += denominations[1];
   });
-  if (sum_of_cash_register == change_Due) {
-    return { status: 'CLOSED', change: cid };
+
+  if (sumCashRegister == changeDue) {
+    return { status: 'CLOSED', change: cashDrawerArr };
   }
 
-  if (price > cash) {
+  if (purchasePrice > cashReceived) {
     return { status: 'INSUFFICENT_FUNDS', change: [] };
   }
-  if (price === cash) {
+  if (purchasePrice === cashReceived) {
     return { status: 'OPEN', change: [] };
   }
 
-  const cashRegister = (denominatorNumber, writtenDenominator, cidIndex) => {
-    console.log(
-      'Denominator ',
-      denominatorNumber,
-      'Written ',
-      writtenDenominator
-    );
-    // remainder change to be calculated
-    temp_Change = (change_Due % denominatorNumber).toFixed(2);
+  const CashRegister = (denomination, denominatorString, denominatorIndex) => {
+    console.log(denomination, ' ', denominatorString, ' ', denominatorIndex);
     // How many potential denominator can we remove from change total
-    temp_Multiple = Math.floor(change_Due / denominatorNumber);
-    // How many multiples of currenty denominator are currentlly in the cash register
-    let how_Many_Current_Multiples_In_Cash_Register =
-      cid[cidIndex][1] / denominatorNumber;
-    console.log(how_Many_Current_Multiples_In_Cash_Register);
+    let denominationMultiples = Math.floor(changeDue / denomination);
+    // How many multiples of currenty denominator are currentlly in the cashReceived register
+    let howManyCurrentMultiplesInCashRegister =
+      cashDrawerArr[denominatorIndex][1] / denomination;
+    // console.log(howManyCurrentMultiplesInCashRegister);
     // Not Enough 100 Dollar Bills In Cash Register
-    if (how_Many_Current_Multiples_In_Cash_Register < temp_Multiple) {
-      change_Return_Object.change.push([
-        writtenDenominator,
-        how_Many_Current_Multiples_In_Cash_Register * denominatorNumber,
+    if (howManyCurrentMultiplesInCashRegister < denominationMultiples) {
+      STATUS_AND_CHANGE_DUE.change.push([
+        denominatorString,
+        howManyCurrentMultiplesInCashRegister * denomination,
       ]);
-      change_Due =
-        change_Due -
-        (
-          how_Many_Current_Multiples_In_Cash_Register * denominatorNumber
-        ).toFixed(2);
-      console.log('Change due: ', change_Due);
+      changeDue =
+        changeDue -
+        (howManyCurrentMultiplesInCashRegister * denomination).toFixed(2);
+      // console.log('Change due: ', changeDue);
     }
     // Enough 100 Multiples in Cash Register for Change
-    if (how_Many_Current_Multiples_In_Cash_Register >= temp_Multiple) {
-      change_Return_Object.change.push([
-        writtenDenominator,
-        temp_Multiple * denominatorNumber,
+    if (howManyCurrentMultiplesInCashRegister >= denominationMultiples) {
+      STATUS_AND_CHANGE_DUE.change.push([
+        denominatorString,
+        denominationMultiples * denomination,
       ]);
-      change_Due = (change_Due - temp_Multiple * denominatorNumber).toFixed(2);
-      console.log('Change due: ', change_Due);
+      changeDue = (changeDue - denominationMultiples * denomination).toFixed(2);
+      // console.log('Change due: ', changeDue);
     }
   };
+  let index = 0;
+  // LEGAL_TENDER_ARR.forEach((denominations) => {
+  //   // console.log(changeDue);
+  //   if (changeDue / denominations[1] >= 1) {
+  //     // console.log(denominations[0]);
+  //     // console.log(denominations[1]);
+  //     // console.log(index);
+  //     CashRegister(denominations[1], denominations[0], index);
+  //   }
+  //   index += 1;
+  // });
 
-  if (change_Due / 100 >= 1) {
-    denominatorNumber = 100;
-    writtenDenominator = 'ONE HUNDRED';
-    cidIndex = 8;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
-  }
-
-  if (change_Due / 20 >= 1) {
-    denominatorNumber = 20;
-    writtenDenominator = 'TWENTY';
-    cidIndex = 7;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
-  }
-  if (change_Due / 10 >= 1) {
-    denominatorNumber = 10;
-    writtenDenominator = 'TEN';
-    cidIndex = 6;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
-  }
-  if (change_Due / 5 >= 1) {
-    denominatorNumber = 5;
-    writtenDenominator = 'FIVE';
-    cidIndex = 5;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
+  if (changeDue / 100 >= 1) {
+    let denomination = 100;
+    let denominatorString = 'ONE HUNDRED';
+    let denominatorIndex = 8;
+    CashRegister(denomination, denominatorString, denominatorIndex);
   }
 
-  if (change_Due / 1 >= 1) {
-    denominatorNumber = 1;
-    writtenDenominator = 'ONE';
-    cidIndex = 4;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
+  if (changeDue / 20 >= 1) {
+    let denomination = 20;
+    let denominatorString = 'TWENTY';
+    let denominatorIndex = 7;
+    CashRegister(denomination, denominatorString, denominatorIndex);
+  }
+  if (changeDue / 10 >= 1) {
+    let denomination = 10;
+    let denominatorString = 'TEN';
+    let denominatorIndex = 6;
+    CashRegister(denomination, denominatorString, denominatorIndex);
+  }
+  if (changeDue / 5 >= 1) {
+    let denomination = 5;
+    let denominatorString = 'FIVE';
+    let denominatorIndex = 5;
+    CashRegister(denomination, denominatorString, denominatorIndex);
   }
 
-  if (change_Due / 0.25 >= 1) {
-    denominatorNumber = 0.25;
-    writtenDenominator = 'QUARTER';
-    cidIndex = 3;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
-  }
-  if (change_Due / 0.1 >= 1) {
-    denominatorNumber = 0.1;
-    writtenDenominator = 'DIME';
-    cidIndex = 2;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
+  if (changeDue / 1 >= 1) {
+    let denomination = 1;
+    let denominatorString = 'ONE';
+    let denominatorIndex = 4;
+    CashRegister(denomination, denominatorString, denominatorIndex);
   }
 
-  if (change_Due / 0.05 >= 1) {
-    denominatorNumber = 0.05;
-    writtenDenominator = 'NICKEL';
-    cidIndex = 1;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
+  if (changeDue / 0.25 >= 1) {
+    let denomination = 0.25;
+    let denominatorString = 'QUARTER';
+    let denominatorIndex = 3;
+    CashRegister(denomination, denominatorString, denominatorIndex);
+  }
+  if (changeDue / 0.1 >= 1) {
+    let denomination = 0.1;
+    let denominatorString = 'DIME';
+    let denominatorIndex = 2;
+    CashRegister(denomination, denominatorString, denominatorIndex);
   }
 
-  if (change_Due / 0.01 >= 1) {
-    denominatorNumber = 0.01;
-    writtenDenominator = 'PENNY';
-    cidIndex = 0;
-    cashRegister(denominatorNumber, writtenDenominator, cidIndex);
+  if (changeDue / 0.05 >= 1) {
+    let denomination = 0.05;
+    let denominatorString = 'NICKEL';
+    let denominatorIndex = 1;
+    CashRegister(denomination, denominatorString, denominatorIndex);
   }
 
-  if (change_Due != 0) {
+  if (changeDue / 0.01 >= 1) {
+    let denomination = 0.01;
+    let denominatorString = 'PENNY';
+    let denominatorIndex = 0;
+    CashRegister(denomination, denominatorString, denominatorIndex);
+  }
+
+  if (changeDue != 0) {
     return { status: 'INSUFFICIENT_FUNDS', change: [] };
   }
 
-  console.log('Return Object ', change_Return_Object);
+  // console.log('Return Object ', STATUSANDCHANGEDUEOBJECT);
+  // console.log(STATUS_AND_CHANGE_DUE);
 
-  return change_Return_Object;
+  return STATUS_AND_CHANGE_DUE;
 }
 
 checkCashRegister(3.26, 100, [
@@ -143,3 +152,18 @@ checkCashRegister(3.26, 100, [
 ]);
 
 module.exports = checkCashRegister;
+
+/*
+{
+      status: 'OPEN',
+      change: [
+        ['TWENTY', 60],
+        ['TEN', 20],
+        ['FIVE', 15],
+        ['ONE', 1],
+        ['QUARTER', 0.5],
+        ['DIME', 0.2],
+        ['PENNY', 0.04],
+      ],
+    }
+*/
--- cashregisterproject05/refactor.js
+++ cashregisterproject05/refactor.js
@@ -1,122 +1,122 @@
-function checkCashRegister(price, cash, cid) {
-  let change_Return_Object = { status: 'OPEN', change: [] };
-  let change_Due = cash - price;
-  let temp_Change;
-  let temp_Multiple;
-  let sum_of_cash_register = 0;
-  let denominatorNumber;
-  let writtenDenominator = '';
+// function checkCashRegister(price, cash, cid) {
+//   let change_Return_Object = { status: 'OPEN', change: [] };
+//   let change_Due = cash - price;
+//   let temp_Change;
+//   let temp_Multiple;
+//   let sum_of_cash_register = 0;
+//   let denominatorNumber;
+//   let writtenDenominator = '';
 
-  cid.forEach((element) => {
-    sum_of_cash_register = sum_of_cash_register += element[1];
-  });
-  if (sum_of_cash_register == change_Due) {
-    return { status: 'CLOSED', change: cid };
-  }
+//   cid.forEach((element) => {
+//     sum_of_cash_register = sum_of_cash_register += element[1];
+//   });
+//   if (sum_of_cash_register == change_Due) {
+//     return { status: 'CLOSED', change: cid };
+//   }
 
-  if (price > cash) {
-    return { status: 'INSUFFICENT_FUNDS', change: [] };
-  }
-  if (price === cash) {
-    return { status: 'OPEN', change: [] };
-  }
+//   if (price > cash) {
+//     return { status: 'INSUFFICENT_FUNDS', change: [] };
+//   }
+//   if (price === cash) {
+//     return { status: 'OPEN', change: [] };
+//   }
 
-  const cashRegister = (denominatorNumber, writtenDenominator) => {
-    // remainder change to be calculated
-    temp_Change = (change_Due % denominatorNumber).toFixed(2);
-    // How many potential 100 can we remove from change total
-    temp_Multiple = Math.floor(change_Due / denominatorNumber);
-    // How many multiples of 100 are currentlly in the cash register
-    let how_Many_Current_Multiples_In_Cash_Register =
-      cid[8][1] / denominatorNumber;
-    // Not Enough 100 Dollar Bills In Cash Register
-    if (how_Many_Current_Multiples_In_Cash_Register < temp_Multiple) {
-      change_Return_Object.change.push([
-        writtenDenominator,
-        how_Many_Current_Multiples_In_Cash_Register * denominatorNumber,
-      ]);
-      change_Due =
-        change_Due -
-        how_Many_Current_Multiples_In_Cash_Register * denominatorNumber;
-    }
-    // Enough 100 Multiples in Cash Register for Change
-    if (how_Many_Current_Multiples_In_Cash_Register >= temp_Multiple) {
-      change_Return_Object.change.push([
-        writtenDenominator,
-        temp_Multiple * denominatorNumber,
-      ]);
-      change_Due = change_Due - temp_Multiple * denominatorNumber;
-    }
-  };
+//   const cashRegister = (denominatorNumber, writtenDenominator) => {
+//     // remainder change to be calculated
+//     temp_Change = (change_Due % denominatorNumber).toFixed(2);
+//     // How many potential 100 can we remove from change total
+//     temp_Multiple = Math.floor(change_Due / denominatorNumber);
+//     // How many multiples of 100 are currentlly in the cash register
+//     let how_Many_Current_Multiples_In_Cash_Register =
+//       cid[8][1] / denominatorNumber;
+//     // Not Enough 100 Dollar Bills In Cash Register
+//     if (how_Many_Current_Multiples_In_Cash_Register < temp_Multiple) {
+//       change_Return_Object.change.push([
+//         writtenDenominator,
+//         how_Many_Current_Multiples_In_Cash_Register * denominatorNumber,
+//       ]);
+//       change_Due =
+//         change_Due -
+//         how_Many_Current_Multiples_In_Cash_Register * denominatorNumber;
+//     }
+//     // Enough 100 Multiples in Cash Register for Change
+//     if (how_Many_Current_Multiples_In_Cash_Register >= temp_Multiple) {
+//       change_Return_Object.change.push([
+//         writtenDenominator,
+//         temp_Multiple * denominatorNumber,
+//       ]);
+//       change_Due = change_Due - temp_Multiple * denominatorNumber;
+//     }
+//   };
 
-  if (change_Due / 100 >= 1) {
-    denominatorNumber = 100;
-    writtenDenominator = 'ONE HUNDRED';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
+//   if (change_Due / 100 >= 1) {
+//     denominatorNumber = 100;
+//     writtenDenominator = 'ONE HUNDRED';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
 
-  if (change_Due / 20 >= 1) {
-    denominatorNumber = 20;
-    writtenDenominator = 'TWENTY';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
-  if (change_Due / 10 >= 1) {
-    denominatorNumber = 10;
-    writtenDenominator = 'TEN';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
-  if (change_Due / 5 >= 1) {
-    denominatorNumber = 5;
-    writtenDenominator = 'FIVE';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
+//   if (change_Due / 20 >= 1) {
+//     denominatorNumber = 20;
+//     writtenDenominator = 'TWENTY';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
+//   if (change_Due / 10 >= 1) {
+//     denominatorNumber = 10;
+//     writtenDenominator = 'TEN';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
+//   if (change_Due / 5 >= 1) {
+//     denominatorNumber = 5;
+//     writtenDenominator = 'FIVE';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
 
-  if (change_Due / 1 >= 1) {
-    denominatorNumber = 1;
-    writtenDenominator = 'ONE';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
+//   if (change_Due / 1 >= 1) {
+//     denominatorNumber = 1;
+//     writtenDenominator = 'ONE';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
 
-  if (change_Due / 0.25 >= 1) {
-    denominatorNumber = 0.25;
-    writtenDenominator = 'QUARTER';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
-  if (change_Due / 0.1 >= 1) {
-    denominatorNumber = 0.1;
-    writtenDenominator = 'DIME';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
+//   if (change_Due / 0.25 >= 1) {
+//     denominatorNumber = 0.25;
+//     writtenDenominator = 'QUARTER';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
+//   if (change_Due / 0.1 >= 1) {
+//     denominatorNumber = 0.1;
+//     writtenDenominator = 'DIME';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
 
-  if (change_Due / 0.05 >= 1) {
-    denominatorNumber = 0.05;
-    writtenDenominator = 'NICKEL';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
+//   if (change_Due / 0.05 >= 1) {
+//     denominatorNumber = 0.05;
+//     writtenDenominator = 'NICKEL';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
 
-  if (change_Due / 0.01 >= 1) {
-    denominatorNumber = 0.01;
-    writtenDenominator = 'PENNY';
-    cashRegister(denominatorNumber, writtenDenominator);
-  }
+//   if (change_Due / 0.01 >= 1) {
+//     denominatorNumber = 0.01;
+//     writtenDenominator = 'PENNY';
+//     cashRegister(denominatorNumber, writtenDenominator);
+//   }
 
-  if (change_Due != 0) {
-    return { status: 'INSUFFICIENT_FUNDS', change: [] };
-  }
+//   if (change_Due != 0) {
+//     return { status: 'INSUFFICIENT_FUNDS', change: [] };
+//   }
 
-  return change_Return_Object;
-}
+//   return change_Return_Object;
+// }
 
-checkCashRegister(19.5, 105, [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100],
-]);
+// checkCashRegister(19.5, 105, [
+//   ['PENNY', 1.01],
+//   ['NICKEL', 2.05],
+//   ['DIME', 3.1],
+//   ['QUARTER', 4.25],
+//   ['ONE', 90],
+//   ['FIVE', 55],
+//   ['TEN', 20],
+//   ['TWENTY', 60],
+//   ['ONE HUNDRED', 100],
+// ]);
 
-module.exports = checkCashRegister;
+// module.exports = checkCashRegister;
