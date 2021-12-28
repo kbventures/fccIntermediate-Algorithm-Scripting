40b5e859ef277a0b70384799680fc757f2f521db
kbventures <kmb_x@hotmail.com>
2021-12-28 05:36:48 +0200

    efactored legal tender in order to reduce code

--- cashregisterproject05/cashregister.js
+++ cashregisterproject05/cashregister.js
@@ -29,112 +29,46 @@ function checkCashRegister(purchasePrice, cashReceived, cashDrawerArr) {
   }
 
   const CashRegister = (denomination, denominatorString, denominatorIndex) => {
-    console.log(denomination, ' ', denominatorString, ' ', denominatorIndex);
     // How many potential denominator can we remove from change total
-    let denominationMultiples = Math.floor(changeDue / denomination);
+    const DENOMINATION_MULTIPLES = Math.floor(changeDue / denomination);
     // How many multiples of currenty denominator are currentlly in the cashReceived register
-    let howManyCurrentMultiplesInCashRegister =
+    const HOW_MANY_CURRENT_MULTIPLES_CASH_IN_REGISTER =
       cashDrawerArr[denominatorIndex][1] / denomination;
-    // console.log(howManyCurrentMultiplesInCashRegister);
+    console.log(HOW_MANY_CURRENT_MULTIPLES_CASH_IN_REGISTER);
     // Not Enough 100 Dollar Bills In Cash Register
-    if (howManyCurrentMultiplesInCashRegister < denominationMultiples) {
+    if (HOW_MANY_CURRENT_MULTIPLES_CASH_IN_REGISTER < DENOMINATION_MULTIPLES) {
       STATUS_AND_CHANGE_DUE.change.push([
         denominatorString,
-        howManyCurrentMultiplesInCashRegister * denomination,
+        HOW_MANY_CURRENT_MULTIPLES_CASH_IN_REGISTER * denomination,
       ]);
       changeDue =
         changeDue -
-        (howManyCurrentMultiplesInCashRegister * denomination).toFixed(2);
+        (HOW_MANY_CURRENT_MULTIPLES_CASH_IN_REGISTER * denomination).toFixed(2);
       // console.log('Change due: ', changeDue);
     }
     // Enough 100 Multiples in Cash Register for Change
-    if (howManyCurrentMultiplesInCashRegister >= denominationMultiples) {
+    if (HOW_MANY_CURRENT_MULTIPLES_CASH_IN_REGISTER >= DENOMINATION_MULTIPLES) {
       STATUS_AND_CHANGE_DUE.change.push([
         denominatorString,
-        denominationMultiples * denomination,
+        DENOMINATION_MULTIPLES * denomination,
       ]);
-      changeDue = (changeDue - denominationMultiples * denomination).toFixed(2);
+      changeDue = (changeDue - DENOMINATION_MULTIPLES * denomination).toFixed(
+        2
+      );
       // console.log('Change due: ', changeDue);
     }
   };
-  let index = 0;
-  // LEGAL_TENDER_ARR.forEach((denominations) => {
-  //   // console.log(changeDue);
-  //   if (changeDue / denominations[1] >= 1) {
-  //     // console.log(denominations[0]);
-  //     // console.log(denominations[1]);
-  //     // console.log(index);
-  //     CashRegister(denominations[1], denominations[0], index);
-  //   }
-  //   index += 1;
-  // });
-
-  if (changeDue / 100 >= 1) {
-    let denomination = 100;
-    let denominatorString = 'ONE HUNDRED';
-    let denominatorIndex = 8;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-
-  if (changeDue / 20 >= 1) {
-    let denomination = 20;
-    let denominatorString = 'TWENTY';
-    let denominatorIndex = 7;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-  if (changeDue / 10 >= 1) {
-    let denomination = 10;
-    let denominatorString = 'TEN';
-    let denominatorIndex = 6;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-  if (changeDue / 5 >= 1) {
-    let denomination = 5;
-    let denominatorString = 'FIVE';
-    let denominatorIndex = 5;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-
-  if (changeDue / 1 >= 1) {
-    let denomination = 1;
-    let denominatorString = 'ONE';
-    let denominatorIndex = 4;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-
-  if (changeDue / 0.25 >= 1) {
-    let denomination = 0.25;
-    let denominatorString = 'QUARTER';
-    let denominatorIndex = 3;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-  if (changeDue / 0.1 >= 1) {
-    let denomination = 0.1;
-    let denominatorString = 'DIME';
-    let denominatorIndex = 2;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-
-  if (changeDue / 0.05 >= 1) {
-    let denomination = 0.05;
-    let denominatorString = 'NICKEL';
-    let denominatorIndex = 1;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
-
-  if (changeDue / 0.01 >= 1) {
-    let denomination = 0.01;
-    let denominatorString = 'PENNY';
-    let denominatorIndex = 0;
-    CashRegister(denomination, denominatorString, denominatorIndex);
-  }
+  let index = 8;
+  LEGAL_TENDER_ARR.forEach((denominations) => {
+    if (changeDue / denominations[1] >= 1) {
+      CashRegister(denominations[1], denominations[0], index);
+    }
+    index -= 1;
+  });
 
   if (changeDue != 0) {
     return { status: 'INSUFFICIENT_FUNDS', change: [] };
   }
-
-  // console.log('Return Object ', STATUSANDCHANGEDUEOBJECT);
-  // console.log(STATUS_AND_CHANGE_DUE);
 
   return STATUS_AND_CHANGE_DUE;
 }
